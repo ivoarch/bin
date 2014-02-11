282c51f98915a167b9f40a827e4ffea7af96c5c2
ivoarch <ivkuzev@gmail.com>
2014-02-11 09:26:42 +0100

    Update scripts

--- system/setup_git.sh
+++ system/setup_git.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+if [ ! $# -eq 2 ];
+then
+    echo "usage: sh $0 user_name user_mail"
+    exit;
+fi
+
 pacman -S git
 
-git config --global user.name "ivoarch"
+user_name=$1
+usere_mail=$2
 
-git config --global user.email "ivkuzev@gmail.com"
+git config --global user.name $user_name
+
+git config --global user.email $user_mail
 
 git config --global credential.helper cache
 
--- system/symlinkall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0
-ln -s ~/.dotfiles/.xinitrc ~/.xinitrc
-ln -s ~/.dotfiles/.xmobarrc ~/.xmobarrc
-ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
-ln -s ~/.dotfiles/.ratpoisonrc ~/.ratpoisonrc
-ln -s ~/.dotfiles/.dircolors ~/.dircolors
