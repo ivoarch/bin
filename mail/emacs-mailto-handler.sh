738a946c18049502f0c585f4b91d722561ae40b7
ivoarch <ivkuzev@gmail.com>
2014-02-11 19:49:28 +0100

    Update emacs script

--- mail/emacs-mailto-handler.sh
+++ mail/emacs-mailto-handler.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 # emacs-mailto-handler
-# <http://permalink.gmane.org/gmane.mail.mu.general/974>
 
-mailto=$1
-mailto="mailto:${mailto#mailto:}"
-mailto=$(printf '%s\n' "$mailto" | sed -e 's/[\"]/\\&/g')
-elisp_expr="(mailto-compose-mail  \"$mailto\")"
+elisp_expr="(compose-mail \"${1#mailto:}\")"
 
-# replace mu4e-compose-new/browse-url with mailto-compose-mail if problems
-
-emacsclient -a "" -c -n --eval "$elisp_expr" \
+emacsclient -c --no-wait --eval "$elisp_expr" \
     '(set-window-dedicated-p (selected-window) t)'
