09fa4991c221fc45f148301b46c8f2c9d6b3b324
ivoarch <ivkuzev@gmail.com>
2014-04-19 23:30:04 +0200

    Adding a new scripts

---
+++ Centos/emacs-keybindings-in-firefox.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Hardcore hack for making firefox more emacs-like:
+
+wget https://github.com/mooz/keysnail/raw/master/keysnail.xpi
+firefox keysnail.xpi
---
+++ Centos/gtk-key-theme-emacs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Emacs keybindigs in Centos (Gnome)
+
+gconftool-2 --set /desktop/gnome/interface/gtk_key_theme Emacs --type string
+
+exit
