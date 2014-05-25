fa780d40c04d23ca531d0049ec0aef691cd4c9ab
ivoarch <ivkuzev@gmail.com>
2014-05-25 20:28:25 +0200

    Add todo

--- Centos/more.sh
+++ Centos/more.sh
@@ -2,7 +2,7 @@
 # More opcional apps for (Centos 6...)
 
 # Administration
-yum install -y gnome-system-monitor htop ps_mem seahorse seahorse-plugins keepassx
+yum install -y gnome-system-monitor htop ps_mem seahorse seahorse-plugins keepassx system-config-language
 
 # Accessories
 yum install -y gcalctool gnote file-roller
@@ -22,11 +22,14 @@ yum install -y inkscape gimp blender
 # Gnome-tools
 yum install -y alacarte gconf-editor gnome-utils
 
+# Hardware-tools
+yum install -y ls_sensors hddtemp
+
 # Games
 yum install -y gnome-games gnome-games-extra fortune-mod cowsay
 
 # Server
-yum install -y sshpass filezilla vsftpd httpd
+yum install -y sshpass filezilla vsftpd httpd noip
 
 # Media
 yum install -y audacity brasero vlc npapi-vlc rhythmbox mplayer mencoder ffmpeg gtk-recordmydesktop
