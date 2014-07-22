6dbdb7e34cc9275d707d99f8577513fa5ce252eb
Ivaylo Kuzev <ivkuzev@gmail.com>
2014-07-22 10:48:40 +0200

    Firefox bg

---
+++ Centos/firefox_bg.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Firefox на български.
+
+# Firefox version
+version=24.6.0esr
+
+echo "Downloading Firefox-$version..."
+cd /usr/local/src
+# 32-bit
+wget ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/linux-i686/bg/firefox-${version}.tar.bz2
+# 64-bit
+#wget ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/linux-x86_64/bg/firefox-${version}.tar.bz2
+echo "Installing Firefox-$version..."
+tar -xjvf firefox-${version}.tar.bz2
+rm -rf /opt/firefox
+mv firefox /opt/firefox
+rm /usr/bin/firefox
+ln -s /opt/firefox/firefox /usr/bin/firefox
+echo "Cleaning up..."
+rm -rf firefox-${version} firefox-${version}.tar.bz2
+
+exit
